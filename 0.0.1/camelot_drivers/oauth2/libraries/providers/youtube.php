413bfddc3ad64c235da45956032e4044a975faef
Timothy Seebus <Timothyseebus@hotmail.com>
2012-03-18 18:40:20 +0000

    added partially working youtube support need work on get_user/data function might actually add this to the google plug in

---
+++ 0.0.1/camelot_drivers/oauth2/libraries/providers/youtube.php
@@ -0,0 +1,55 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+/*
+ *  Twsweb Network
+ * 
+ *  The codebase for the Twsweb Network sites
+ * 
+ *  @package         Twsweb Network
+ *  @author          Timothy Seebus <Timothyseebus@twsweb-int.com>
+ *  @copyright       2012, Timothy Seebus
+ *  @link            http://labs.Twsweb-int.com/network
+ *  @filesource
+ */
+
+/**
+ * Inspiration Taken from Phil Sturgeons codeigniter-oauth2 spark (https://github.com/philsturgeon/codeigniter-oauth2); 
+ *	
+ *
+ * @subpackage camelot_auth
+ */
+class Oauth2_Provider_Youtube extends Oauth2_provider
+{
+	public function __Construct($driver)
+	{
+		$this->provider_name = 'Youtube';
+		parent::__Construct($driver);
+		$this->method = 'POST';
+	}
+
+	public function get_scope()
+	{
+		$scope = array('https://www.google.com/m8/feeds','http://gdata.youtube.com');
+		/*foreach ($this->CI->config->item('Youtube_Permissions') as $premission => $value) {
+			if($value === TRUE){
+				$premission = 'https://www.googleapis.com/auth/userinfo.'.$premission;
+				array_push($scope,$premission);
+			}
+		}*/
+		if(!empty($scope) && is_array($scope)){
+			$scope = implode(' ', $scope);
+		}
+		
+		return $scope;
+	}
+
+	public function get_user($access_token)
+	{
+		$api_url = $this->api_endpoint.'?alt=json&'.http_build_query(array('access_token' => $access_token));
+		
+		$userdata = json_decode(file_get_contents($api_url));
+		return $userdata;
+	}
+}
