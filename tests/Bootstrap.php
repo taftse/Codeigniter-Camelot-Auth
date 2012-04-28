420cafc8e9ed8cf4d3c675f5af6399187c0f3e98
Timothy Seebus <Timothyseebus@hotmail.com>
2012-04-28 17:47:53 +0100

    another try

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,7 +1,3 @@
 <?php 
-
-include_once(realpath('../../vendor/Codeigniter/tests/Bootstrap.php'));
 include_once(realpath(__DIR__.DIRECTORY_SEPARATOR.'../vendor/Codeigniter/tests/Bootstrap.php'));
-
-
 ?>
