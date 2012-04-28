a8d0be312e5c22a020a4935995ad87637143aa2b
Timothy Seebus <Timothyseebus@hotmail.com>
2012-04-28 17:26:30 +0100

    Bootstrap.php update

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,6 +1,5 @@
 <?php 
-
-include_once('../vendor/Codeigniter/tests/Bootstrap.php');
-
+ include_once(realpath('../../vendor/Codeigniter/tests/Bootstrap.php'));
+<toopay> try : include_once(realpath(__DIR__.DIRECTORY_SEPARATOR.'../vendor/Codeigniter/tests/Bootstrap.php'));
 
 ?>
