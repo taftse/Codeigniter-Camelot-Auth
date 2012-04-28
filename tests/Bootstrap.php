384b04ce2415a896f598dd95330c75768126e32d
Timothy Seebus <Timothyseebus@hotmail.com>
2012-04-28 17:54:49 +0100

    Grr Caps

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,3 +1,3 @@
 <?php 
-include_once(realpath(__DIR__.DIRECTORY_SEPARATOR.'../vendor/Codeigniter/tests/Bootstrap.php'));
+include_once(realpath(__DIR__.DIRECTORY_SEPARATOR.'../vendor/CodeIgniter/tests/Bootstrap.php'));
 ?>
