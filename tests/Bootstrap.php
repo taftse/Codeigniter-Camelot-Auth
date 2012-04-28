d962e029022f4842581be2b91e8c81c51144df8e
Timothy Seebus <Timothyseebus@hotmail.com>
2012-04-28 17:43:45 +0100

    lets try again updated Bootstrap.php

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,10 +1,7 @@
 <?php 
-<<<<<<< HEAD
- include_once(realpath('../../vendor/Codeigniter/tests/Bootstrap.php'));
-<toopay> try : include_once(realpath(__DIR__.DIRECTORY_SEPARATOR.'../vendor/Codeigniter/tests/Bootstrap.php'));
-=======
-realpath('../../vendor/Codeigniter/tests/Bootstrap.php');
 
->>>>>>> 5f8684368acb4c59e629c08dbed493ba098ca0e6
+include_once(realpath('../../vendor/Codeigniter/tests/Bootstrap.php'));
+include_once(realpath(__DIR__.DIRECTORY_SEPARATOR.'../vendor/Codeigniter/tests/Bootstrap.php'));
+
 
 ?>
